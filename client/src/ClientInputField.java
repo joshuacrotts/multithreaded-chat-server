d246a0e63ca866705dd8b5251a2ecafadf335724
Joshua Crotts <joshuacrotts@yahoo.com>
2021-05-04 00:54:14 -0400

    Added preliminary history feature to input field.

--- client/src/ClientInputField.java
+++ client/src/ClientInputField.java
@@ -5,7 +5,10 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -26,10 +29,21 @@ public class ClientInputField extends JTextField {
     /**
      *
      */
+    private final Vector<String> HISTORY;
+
+    /**
+     *
+     */
     private JTextFieldAutoComplete autocomplete;
+
+    /**
+     *
+     */
+    private int historyIndex = -1;
 
     public ClientInputField(ClientFrame frame) {
         this.INPUT_QUEUE = new LinkedBlockingQueue<>();
+        this.HISTORY = new Vector<>();
         this.addEnterKeyListener();
         this.autocomplete = new JTextFieldAutoComplete(this, COMMANDS);
         this.setComponentPopupMenu(this.autocomplete);
@@ -43,9 +57,23 @@ public class ClientInputField extends JTextField {
         this.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
+                // If they press enter, then submit the input to the queue.
                 if (e.getKeyCode() == KeyEvent.VK_ENTER) {
                     INPUT_QUEUE.offer(getText());
+                    if (INPUT_QUEUE.peek() != null) {
+                        HISTORY.add(INPUT_QUEUE.peek());
+                        historyIndex = HISTORY.size() - 1;
+                    }
                     setText("");
+                }
+                // If they press up, scroll through the previous inputs.
+                else if (e.getKeyCode() == KeyEvent.VK_UP && (historyIndex - 1) >= 0) {
+                    setText(HISTORY.get(historyIndex));
+                    historyIndex--;
+                }
+                // If they press the down button, scroll through the NEXT inputs, given that they exist.
+                else if (e.getKeyCode() == KeyEvent.VK_DOWN && (historyIndex + 1) < HISTORY.size()) {
+                    setText(HISTORY.get(++historyIndex));
                 }
             }
 
